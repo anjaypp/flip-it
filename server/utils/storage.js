6371445eb43b89ef082464ec34f0ee1e3b67514b
Anjay P.P <anjaypp20@gmail.com>
2024-12-10 14:17:01 +0530

    made changes to storage.js and fixed the compatilbility issue with cloudinary and multer-storage-cloudinary

--- server/utils/storage.js
+++ server/utils/storage.js
@@ -1,4 +1,4 @@
-const {CloudinaryStorage} = require('multer-storage-cloudinary');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('../config/cloudinary');
 
 const storage = new CloudinaryStorage({
